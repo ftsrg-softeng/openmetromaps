7a1330033362ef18dea0c393b5941e6acc47733d
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2024-09-19 09:54:50 +0000

    Update script (fixes #1)

--- scripts/openmetromaps-cli.sh
+++ scripts/openmetromaps-cli.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-files=($(dirname $0)/../subprojects/cli/target/openmetromaps-cli-*-jar-with-dependencies.jar)
+scriptdir=$(dirname "$0")
+files=$(find "$scriptdir/../subprojects/cli/target/" -name 'openmetromaps-cli-*-jar-with-dependencies.jar')
 
 if [ ! -e "${files[0]}" ]; then
-	echo "Please run 'mvn package'"
-	exit 1
+    echo "Please run 'mvn package'"
+    exit 1
 fi
 
 exec java -jar "${files[0]}" "$@"
